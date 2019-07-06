cac5afe8eb5f799f56b1bf600052073a591798aa
VIREAK KEOSOKVIBOL <vireakkeosokvibol@bondom.co>
2019-07-06 13:52:47 +0700

    Add layouts.
    Develop a reuse layouts using shared module.
    
    Now you can call <app-header> and <app-footer> by importing a LayoutModule.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { LayoutsModule } from '../layouts/layouts.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    LayoutsModule,
     RouterModule.forChild([
       {
         path: '',
